cfdfba58ece1c946a7372817d0534f22d828da51
Yuliya <cisan@ukr.net>
2020-07-25 00:30:00 +0300

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -51,6 +51,23 @@ class User < ActiveRecord::Base
     UserMailer.account_activation(self).deliver_now
   end
 
+# Устанавливает атрибуты для сброса пароля.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Отправляет электронное письмо для сброса пароля.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Возвращает true, если истек срок давности ссылки для сброса пароля .
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
   private
 
     # Переводит адрес электронной почты в нижний регистр.
