ad07c974c46d241b13d1eba2692f1ba1a222742e
Yuliya <cisan@ukr.net>
2020-08-08 01:27:40 +0300

    Add user following part 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -72,6 +81,21 @@ class User < ActiveRecord::Base
   # Полная реализация в "Следовании за пользователями".
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # Начать читать сообщения пользователя.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Перестать читать сообщения пользователя.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Возвращает true, если текущий пользователь читает сообщения другого пользователя.
+  def following?(other_user)
+    following.include?(other_user)
   end
   
   private
